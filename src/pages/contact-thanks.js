b28c8f5af2708d29790c43a43437ca3478f0e313
Anne-Marie Dufour <hello@annemariedufour.com>
2020-10-07 16:30:05 +0200

    Add basic styles to y=thank you page

--- src/pages/contact-thanks.js
+++ src/pages/contact-thanks.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import SocialLinks from '../components/SocialLinks'
 
-const ContactThanks = () => {
-  return <Layout styleClass="contact-page thank-you">
-    <div className="contact-page">
+const Contact = () => {
+  return <Layout styleClass="contact-page">
+    <div className="contact-page thanks">
       <div className="contact-header">
         <div className="logo-bg"></div>
         <div className="container">
@@ -13,11 +14,24 @@ const ContactThanks = () => {
       </div>
       <div className="page-content">
         <div className="container">
-          
+          <div className="contact-info">
+            <section className="contact-form-section">
+              <h2>Thank you!</h2>
+              <div className="contact-form">
+                <div>I will be in touch with you shortly.</div>
+              </div>
+            </section>
+            <section className="contact-connect-section">
+              <h2>Let’s connect!</h2>
+              <div className="contact-connect">
+                <SocialLinks />
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     </div>
   </Layout>
 }
 
-export default ContactThanks
+export default Contact
