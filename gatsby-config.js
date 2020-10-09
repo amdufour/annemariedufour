54c4692b2b951c52eb5a70d3eaef43606dd8652f
Anne-Marie Dufour <hello@annemariedufour.com>
2020-10-09 11:28:15 +0200

    Install gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,19 @@ module.exports = {
         path: `${__dirname}/src/assets/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: ` #F6FDFF`,
+        theme_color: `#07A3B2`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 }
