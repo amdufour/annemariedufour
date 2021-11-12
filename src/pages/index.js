18a3d2c712dd7c3e2fe8b795e86ce6b198b7c253
Anne-Marie Dufour <hello@annemariedufour.com>
2021-11-12 14:37:27 +0100

    Test d3 implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,19 +9,31 @@ import HomeWhat from "../components/home/HomeWhat"
 import HomeProjects from '../components/home/HomeProjects'
 import HomeWho from '../components/home/HomeWho'
 
-export default function Home() {
-  return (
-    <Layout styleClass="header-with-gradient homepage">
-      <SEO />
-      <HomeHero />
-      <div className="home-content">
-        <HomeIntroduction />
-        <HomeWhat />
-        {/* <HomeSpotifyBanner /> */}
-        {/* <HomeWhere /> */}
-        <HomeProjects />
-        <HomeWho />
-      </div>
-    </Layout>
-  )
+import * as d3 from "d3-selection"
+
+class Home extends React.Component {
+
+  componentDidMount() {
+    d3.select('#draggable-line').text('hey there!');
+  }
+
+  render() {
+    return (
+      <Layout styleClass="header-with-gradient homepage">
+        <SEO />
+        <div id="draggable-line"></div>
+        <HomeHero />
+        <div className="home-content">
+          <HomeIntroduction />
+          <HomeWhat />
+          {/* <HomeSpotifyBanner /> */}
+          {/* <HomeWhere /> */}
+          <HomeProjects />
+          <HomeWho />
+        </div>
+      </Layout>
+    )
+  }
 }
+
+export default Home;
